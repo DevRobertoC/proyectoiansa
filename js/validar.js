36f7effa716ffa1c55bc07749284734f57968e3e
SharyPrada <prada.m94@gmail.com>
2022-09-21 01:28:58 -0500

    validacion bien y otras ediciones

--- js/validar.js
+++ js/validar.js
@@ -1,12 +1,15 @@
 function validar(){
-    let usuario=document.formLogin.usuario;
-    let password=document.formLogin.password;
-    let boton=document.formLogin.boton;
+    var user = document.formLogin.usuario.value;
+    var password = document.formLogin.password.value;
+    var nuevaRuta = rutaCambiar() + 'forms/bienvenido.html';
 
-    if (usuario == 'admin' && password == 'admin'){
-        boton.addEventListener('click', function(){
-            window.location.href='nuevoRegistro.html';
-        });
-        
+    if(user == 'admin' && password == 'admin'){
+        window.location.replace(nuevaRuta);
     }
+}
+
+function rutaCambiar(){
+    var absRuta = window.location;
+    var ruta = absRuta.pathname.substring(0, absRuta.pathname.lastIndexOf('pages'));
+    return absRuta.href.substring(0, absRuta.href.length - ((absRuta.pathname + absRuta.search + absRuta.hash).length - ruta.length));
 }
